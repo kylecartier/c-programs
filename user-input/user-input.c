e3e8c27997e8570aa08033a99305d281172046cc
Kyle Cartier <123816695+kylecartier@users.noreply.github.com>
2026-01-11 09:59:39 -0500

    Uploaded file to folder

---
+++ user-input/user-input.c
@@ -0,0 +1,50 @@
+// Header Files
+
+#include <stdio.h>   // For standard input / output functions to function
+#include <string.h>  // For manipulating strings
+
+// Main function
+
+int main()
+
+{
+
+    // Make a variable with a fixed length of 40 characters
+
+    char user_input[40] = "";
+
+    // Prompt the user for intput
+
+    printf("Enter user input: ");
+
+    // Read the user's line of input
+
+    fgets(user_input, sizeof(user_input), stdin);
+
+    // Eliminates the extra line for when the user presses the enter key
+
+    user_input[strlen(user_input) - 1] = '\0';   
+
+    // Print the user's input 
+
+    printf("%s", user_input);
+
+    // This indicates that the program has run sucessfully.
+
+    return 0;
+
+}
+
+// End of Code
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Additional information for Windows and macOS
+
+// Windows:
+// Install gcc on your Linux distribution if needed via Terminal: sudo apt / dnf install gcc -y (pacman -S gcc -y for Arch based distributions)
+// Make an executable on Linux for Windows via gcc utility: x86_64-w64-mingw32-gcc Output.c
+
+// macOS:
+// Make an executable for macOS after downloading it to your Mac via Terminal: gcc Output.c -o Output
+// Make that file executable on macOS via Terminal if it is not already: chmod +x Output
